cff3761b27b08d652089a6d3caa2a97f3657a2d8
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-12 15:12:42 +0530

    Update Main.java

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -7,14 +7,7 @@ import utility.ConnectionManager;
 public class Main {
 	public static void main(String args[]) {
 		
-		Connection con=null;
-		con=ConnectionManager.getConnection();
-		if(con!=null) {
-			System.out.println("Database connection established");
-		}
-		else
-		{
-			System.out.println("Check your connection");
-		}
+	// Fill your code
+		
 	}
 }
