ad7e2d9bd015f79df95629bba51774903528360d
Deepankar <61615789+lemecode@users.noreply.github.com>
2020-05-23 13:23:59 +0530

    FSJ0320B001

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,29 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	
+	public static void main(String args[]) throws ClassNotFoundException, SQLException, IOException {
 		
-	// Fill your code
+	//Object for the Connection
+		
+		Connection con = null;
+		
+	
+		//Object for the ConnectionManager
+		
+		
+		con = ConnectionManager.getConnection();
+		
+		if(con!= null)
+			System.out.println("Connection Established");
+		else
+			System.out.println("Check your connection");
 		
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,52 @@
 package utility;
 
-
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class ConnectionManager {
+	
+
+	public static Properties loadPropertiesFile() throws Exception {
+		Properties prop = new Properties();	
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close(); 
+		return prop;
+	}
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException, IOException {
+		
+		FileReader reader = new FileReader("C:\\eclipse\\java\\lab-jdbc-connection\\src\\jdbc.properties");
+		
+		Properties p = new Properties();
+		
+		p.load(reader);
+		String driver = p.getProperty("driver");
+		String url = p.getProperty("url");
+		String user = p.getProperty("username");
+		String password = p.getProperty("password");
+		
+		//Load Driver
+		
+		Class.forName(driver);
+		
+		//Establish Connection
+		
+		Connection con = null;
+		
+		con = DriverManager.getConnection(url, user, password);
+				
+		return con;
+		
+		
+	}
+	
 	
 	
 }
